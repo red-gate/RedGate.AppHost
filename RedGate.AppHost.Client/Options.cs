0cb7d7deac6523ad9a7f8d3dbc0b8d11283b2bb8
Kevin Boyle <kevin.boyle@outlook.com>
2014-03-06 12:24:09 +0000

    Extract class

---
+++ RedGate.AppHost.Client/Options.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using CommandLine;
+
+namespace RedGate.AppHost.Client
+{
+    internal class Options
+    {
+        [Option('a', "assembly", Required = true, HelpText = "Assembly that contains an IOutOfProcessEntryPoint to load")]
+        public string Assembly { get; set; }
+
+        [Option('i', "id", Required = true, HelpText = "The communication channel to call back to the host")]
+        public string ChannelId { get; set; }
+
+        [Option('d', "debug", Required = false, HelpText = "Opens the client in debug mode")]
+        public bool Debug { get; set; }
+
+        [HelpOption(HelpText = "Display this help screen.")]
+        public string GetUsage()
+        {
+            var usage = new StringBuilder();
+            usage.AppendLine("Red Gate Out of Process App Host");
+            return usage.ToString();
+        }
+    }
+}
--- RedGate.AppHost.Client/Program.cs
+++ RedGate.AppHost.Client/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Threading;
@@ -10,26 +9,6 @@ using RedGate.AppHost.Interfaces;
 
 namespace RedGate.AppHost.Client
 {
-    internal class Options
-    {
-        [Option('a', "assembly", Required = true, HelpText = "Assembly that contains an IOutOfProcessEntryPoint to load")]
-        public string Assembly { get; set; }
-
-        [Option('i', "id", Required = true, HelpText = "The communication channel to call back to the host")]
-        public string ChannelId { get; set; }
-
-        [Option('d', "debug", Required = false, HelpText = "Opens the client in debug mode")]
-        public bool Debug { get; set; }
-
-        [HelpOption(HelpText = "Display this help screen.")]
-        public string GetUsage()
-        {
-            var usage = new StringBuilder();
-            usage.AppendLine("Red Gate Out of Process App Host");
-            return usage.ToString();
-        }
-    }
-
     internal static class Program
     {
         private static SafeChildProcessHandle s_SafeChildProcessHandle;
