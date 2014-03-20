36b3e6fbbc0ad71007e65f5942afbaad3458e4ca
Kevin Boyle <kevin.boyle@red-gate.com>
2014-03-20 15:17:11 +0000

    Move to server, as it is only ever needed by the 'host' application

--- RedGate.AppHost.Server/IChildProcessHandle.cs
+++ RedGate.AppHost.Server/IChildProcessHandle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
+using RedGate.AppHost.Interfaces;
 
-namespace RedGate.AppHost.Interfaces
+namespace RedGate.AppHost.Server
 {
     /// <summary>
     /// A handle to a process, which when initialized, returns a FrameworkElement for rendering
