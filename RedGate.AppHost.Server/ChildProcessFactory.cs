b4a03370b3fb2411096e27fc068b8758d90b5ecc
Andrew Hunter <andrew.hunter@red-gate.com>
2014-09-22 15:47:48 +0100

    Add a new version of the Create() method that can be used to create 64-bit as well as 32-bit child processes. Change from using default parameters to overloads so that the API is binary compatible with the old version.

--- RedGate.AppHost.Server/ChildProcessFactory.cs
+++ RedGate.AppHost.Server/ChildProcessFactory.cs
@@ -2,12 +2,34 @@
 {
     public class ChildProcessFactory
     {
-        public IChildProcessHandle Create(string assemblyName, bool openDebugConsole = false)
+        public IChildProcessHandle Create(string assemblyName, bool openDebugConsole, bool is64Bit)
         {
+            IProcessStartOperation processStarter;
+
+            if (is64Bit)
+            {
+                processStarter = new ProcessStarter64Bit();
+            }
+            else
+            {
+                processStarter = new ProcessStarter32Bit();
+            }
+
             return new RemotedProcessBootstrapper(
                 new StartProcessWithTimeout(
                     new StartProcessWithJobSupport(
-                        new ProcessStarter32Bit()))).Create(assemblyName, openDebugConsole);
+                        processStarter))).Create(assemblyName, openDebugConsole);
+        }
+
+        public IChildProcessHandle Create(string assemblyName, bool openDebugConsole)
+        {
+            // Legacy version of the api
+            return Create(assemblyName, openDebugConsole, false);
+        }
+
+        public IChildProcessHandle Create(string assemblyName)
+        {
+            return Create(assemblyName, false, false);
         }
     }
 }
