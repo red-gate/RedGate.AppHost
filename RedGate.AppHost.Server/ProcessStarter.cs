35526680825f5967a19ab266b18b3647c1556bc0
Adrian Banks <adrian.banks@red-gate.com>
2015-05-06 15:55:52 +0100

    Refactor the process starters to share their common code

---
+++ RedGate.AppHost.Server/ProcessStarter.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace RedGate.AppHost.Server
+{
+    internal abstract class ProcessStarter : IProcessStartOperation
+    {
+        protected abstract string ProcessFileName { get; }
+
+        public Process StartProcess(string assemblyName, string remotingId, bool openDebugConsole = false)
+        {
+            string executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string quotedAssemblyArg = "\"" + Path.Combine(executingDirectory, assemblyName) + "\"";
+
+            var processToStart = Path.Combine(executingDirectory, ProcessFileName);
+            var processArguments = string.Join(" ", new[]
+            {
+                "-i " + remotingId,
+                "-a " + quotedAssemblyArg,
+                openDebugConsole ? "-d" : ""
+            });
+            return Process.Start(processToStart, processArguments);
+        }
+    }
+}
